9f963a55f5c89b5ec4a2aaaead3c7b8f4a82568d
Eric Sayler <esayler@colgate.edu>
2015-02-22 15:07:49 -0500

    change price scale/precision

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221011402) do
+ActiveRecord::Schema.define(version: 20150222195336) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.decimal  "price"
+    t.decimal  "price",                   precision: 10, scale: 2
     t.integer  "minimum_age_appropriate"
     t.integer  "maximum_age_appropriate"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
   end
 
 end
